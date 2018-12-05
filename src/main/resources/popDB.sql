8f4a6089f194a58b451d6e876b56ca99990392be
ZacharyMessersmith <42919583+ZacharyMessersmith@users.noreply.github.com>
2018-12-04 23:09:52 -0500

    Update popDB.sql
    Correcting pop also updates the table to contain numRentalsAvailable

--- src/main/resources/popDB.sql
+++ src/main/resources/popDB.sql
@@ -1,3 +1,4 @@
+
 #fill users
 INSERT INTO users (email,name,phoneNum,password,isMember,isAdmin) VALUES("Bala.Stella@hotmail.com","Bala Stella","3105738768","BobRoss",0,1);
 INSERT INTO users (email,name,phoneNum,password,isMember,isAdmin) VALUES("Sudarshan.Lainey@gmail.com","Sudarshan Lainey","6267653115","BobRoss",0,1);
@@ -14,46 +15,49 @@ INSERT INTO users (email,name,phoneNum,password,isMember,isAdmin) VALUES("Cecili
 INSERT INTO users (email,name,phoneNum,password,isMember,isAdmin) VALUES("Juan.Lainey@yahoo.com","Juan Lainey","5623621092","BobRoss",0,1);
 INSERT INTO users (email,name,phoneNum,password,isMember,isAdmin) VALUES("Elli.Nerthus@hotmail.com","Elli Nerthus","5109072375","BobRoss",0,1);
 INSERT INTO users (email,name,phoneNum,password,isMember,isAdmin) VALUES("Ian.Lainey@yahoo.com","Ian Lainey","6191239302","BobRoss",0,1);
-
 #fill address
-INSERT INTO addresses (street,zip,city,state) VALUES ("New Close Farm Road","24715",Bramwell",IN");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Canon Place","24737",Montcalm",AK");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Fish Rake Lane","24726",Herndon",MN");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Berryhill Avenue","24738",Nemours",WV");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Venette Close","24731",Kegley",MI");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Vandries Street","24701",Bluefield",DE");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Maryfield Park","24732",Kellysville",MO");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Upper Piccadilly","24712",Athens",AZ");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Stoneycrof Crescent","24733",Lashmeet",MD");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Knowle Top Road","24729",Hiawatha",WI");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Merleswen","24716",Bud",IA");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Dolphin Gardens East","24719",Covel",FL");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Benmore View","24724",Freeman",CT");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Kelton Grove","24714",Beeson",AL");
-INSERT INTO addresses (street,zip,city,state) VALUES ("Beechcroft Court","24736",Matoaka",NC");
-
-#fill livesAt1
-
-
-
+INSERT INTO addresses (street,zip,city,state) VALUES ("New Close Farm Road","24715","Bramwell","IN");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Canon Place","24737","Montcalm","AK");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Fish Rake Lane","24726","Herndon","MN");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Berryhill Avenue","24738","Nemours","WV");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Venette Close","24731","Kegley","MI");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Vandries Street","24701","Bluefield","DE");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Maryfield Park","24732","Kellysville","MO");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Upper Piccadilly","24712","Athens","AZ");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Stoneycrof Crescent","24733","Lashmeet","MD");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Knowle Top Road","24729","Hiawatha","WI");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Merleswen","24716","Bud","IA");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Dolphin Gardens East","24719","Covel","FL");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Benmore View","24724","Freeman","CT");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Kelton Grove","24714","Beeson","AL");
+INSERT INTO addresses (street,zip,city,state) VALUES ("Beechcroft Court","24736","Matoaka","NC");
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('280th Ave', '32819', 'WaKeeney', 'KN');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Delaware St', '32852', 'Fanning', 'MI');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Pennsylvania St', '29183', 'Bendena', 'KN');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Burroughs Creek Road', '40212', 'Denton', 'KN');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Lakeside Dr', '23492', 'Highland', 'IN');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Hills Dr', '92092', 'Highland', 'MI');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Meadowoak Ln', '48213', 'Troy', 'NC');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Mansfield Rd', '12523', 'Doniphan', 'NY');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Commonwealth Dr', '24632', 'Small', 'DC');
+INSERT INTO `addresses` (`street`, `zip`, `city`, `state`) VALUES ('Vera Ln', '92301', 'Big', 'TX');
 
 #fill media
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('1', '1942-10-01', 'Funny', 'Slave of Thorn', '5');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('2', '2018-02-11', 'Sad', 'The Heat of the Boy', '3');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('3', '2010-12-30', 'Sad', 'Element of Person', '53');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('4', '2001-01-20', 'Funny', 'The Boy of the Shard', '32');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('5', '1313-11-12', 'Mad', 'Only Servant', '1');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('6', '1111-11-11', 'Mad', 'The Wet Kiss', '43');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('7', '1247-01-02', 'Sad', 'BoyFriend in the Legacy', '0');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('8', '3468-01-03', 'Sad', 'The Princess\'s Vision', '42');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('9', '2249-01-04', 'Happy', 'Scent of Woman', '426');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('10', '1364-01-05', 'Happy', 'Darkest Mists', '76');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('11', '1345-02-01', 'Happy', 'The Crying of Servants', '3');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('12', '5432-02-02', 'Sad', 'Lords in the Luck', '6');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('13', '1903-02-03', 'Mad', 'Blue Staff', '58');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('14', '1801-02-04', 'Mad', 'The Rainbow', '234');
-INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvaliable`) VALUES ('15', '2019-03-01', 'Sad', 'The Rainbow\'s Beginning', '65');
-
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('1', '1942-10-01', 'Funny', 'Slave of Thorn', '5');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('2', '2018-02-11', 'Sad', 'The Heat of the Boy', '3');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('3', '2010-12-30', 'Sad', 'Element of Person', '53');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('4', '2001-01-20', 'Funny', 'The Boy of the Shard', '32');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('5', '1313-11-12', 'Mad', 'Only Servant', '1');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('6', '1111-11-11', 'Mad', 'The Wet Kiss', '43');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('7', '1247-01-02', 'Sad', 'BoyFriend in the Legacy', '0');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('8', '3468-01-03', 'Sad', 'The Princess\'s Vision', '42');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('9', '2249-01-04', 'Happy', 'Scent of Woman', '426');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('10', '1364-01-05', 'Happy', 'Darkest Mists', '76');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('11', '1345-02-01', 'Happy', 'The Crying of Servants', '3');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('12', '5432-02-02', 'Sad', 'Lords in the Luck', '6');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('13', '1903-02-03', 'Mad', 'Blue Staff', '58');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('14', '1801-02-04', 'Mad', 'The Rainbow', '234');
+INSERT INTO `media` (`mediaID`, `releaseDate`, `genre`, `title`, `numCopiesAvailable`) VALUES ('15', '2019-03-01', 'Sad', 'The Rainbow\'s Beginning', '65');
 #fill movies
 INSERT INTO `movies` (`movieID`) VALUES ('1');
 INSERT INTO `movies` (`movieID`) VALUES ('2');
@@ -61,7 +65,6 @@ INSERT INTO `movies` (`movieID`) VALUES ('3');
 INSERT INTO `movies` (`movieID`) VALUES ('4');
 INSERT INTO `movies` (`movieID`) VALUES ('5');
 INSERT INTO `movies` (`movieID`) VALUES ('6');
-
 #fill games
 INSERT INTO `games` (`gameID`, `version`, `platform`) VALUES ('7', '1.0', 'PlayTron 3');
 INSERT INTO `games` (`gameID`, `version`, `platform`) VALUES ('8', '1.12', 'XCar');
@@ -72,7 +75,12 @@ INSERT INTO `games` (`gameID`, `version`, `platform`) VALUES ('12', '12.1', 'Woo
 INSERT INTO `games` (`gameID`, `version`, `platform`) VALUES ('13', '1.01', 'PlayTro');
 INSERT INTO `games` (`gameID`, `version`, `platform`) VALUES ('14', '1.00', 'Wee');
 INSERT INTO `games` (`gameID`, `version`, `platform`) VALUES ('15', '0.12', 'Woo');
-
+#fill plan
+INSERT INTO `plans` (`planID`, `cost`, `numMediaAllowed`) VALUES ('1', '5', '5');
+INSERT INTO `plans` (`planID`, `cost`, `numMediaAllowed`) VALUES ('2', '10', '8');
+INSERT INTO `plans` (`planID`, `cost`, `numMediaAllowed`) VALUES ('3', '15', '10');
+INSERT INTO `plans` (`planID`, `cost`, `numMediaAllowed`) VALUES ('4', '20', '12');
+INSERT INTO `plans` (`planID`, `cost`, `numMediaAllowed`) VALUES ('5', '25', '15');
 #fill has_plan
 INSERT INTO `has_plan` (`email`, `planID`) VALUES ('Bala.Stella@hotmail.com', '3');
 INSERT INTO `has_plan` (`email`, `planID`) VALUES ('Cecilie.Humaira@gmail.com', '2');
@@ -89,16 +97,13 @@ INSERT INTO `has_plan` (`email`, `planID`) VALUES ('Kekepania.Seraphine@yahoo.co
 INSERT INTO `has_plan` (`email`, `planID`) VALUES ('Pavel.America@yahoo.com', '2');
 INSERT INTO `has_plan` (`email`, `planID`) VALUES ('Sudarshan.Lainey@gmail.com', '2');
 INSERT INTO `has_plan` (`email`, `planID`) VALUES ('Zarko.Ghislaine@yahoo.com', '3');
-
 #fill awards
-
 INSERT INTO `awards` (`awardID`, `aname`) VALUES ('1', 'Epic');
 INSERT INTO `awards` (`awardID`, `aname`) VALUES ('2', 'Legendary');
 INSERT INTO `awards` (`awardID`, `aname`) VALUES ('3', 'Trash');
 INSERT INTO `awards` (`awardID`, `aname`) VALUES ('4', 'Meh');
 INSERT INTO `awards` (`awardID`, `aname`) VALUES ('5', 'Good');
 INSERT INTO `awards` (`awardID`, `aname`) VALUES ('6', 'Okay');
-
 #fill rental_info
 INSERT INTO `rental_info` (`rentalID`, `checkoutDate`, `dueDate`, `returnedDate`, `email`, `mediaID`) VALUES ('1', '5090-01-12', '5090-02-12', '5090-01-13', 'Bala.Stella@hotmail.com', '1');
 INSERT INTO `rental_info` (`rentalID`, `checkoutDate`, `dueDate`, `returnedDate`, `email`, `mediaID`) VALUES ('2', '603-12-02', '603-12-14', '603-12-03', 'Bala.Stella@hotmail.com', '2');
@@ -115,11 +120,9 @@ INSERT INTO `rental_info` (`rentalID`, `checkoutDate`, `dueDate`, `returnedDate`
 INSERT INTO `rental_info` (`rentalID`, `checkoutDate`, `dueDate`, `returnedDate`, `email`, `mediaID`) VALUES ('13', '5457-11-16', '5457-12-01', '5457-11-17', 'Geoff.Humaira@gmail.com', '1');
 INSERT INTO `rental_info` (`rentalID`, `checkoutDate`, `dueDate`, `returnedDate`, `email`, `mediaID`) VALUES ('14', '8020-09-18', '8020-10-20', '8020-09-19', 'Geoff.Humaira@gmail.com', '2');
 INSERT INTO `rental_info` (`rentalID`, `checkoutDate`, `dueDate`, `returnedDate`, `email`, `mediaID`) VALUES ('15', '2500-01-13', '2500-05-14', '2500-01-14', 'Geoff.Humaira@gmail.com', '3');
-
 #fill sequel
 INSERT INTO `sequel` (`prequelID`, `sequelID`) VALUES ('1', '2');
 INSERT INTO `sequel` (`prequelID`, `sequelID`) VALUES ('2', '3');
-
 #fill won
 INSERT INTO `won` (`awardID`, `movieID`) VALUES ('3', '1');
 INSERT INTO `won` (`awardID`, `movieID`) VALUES ('1', '2');
@@ -128,4 +131,67 @@ INSERT INTO `won` (`awardID`, `movieID`) VALUES ('5', '4');
 INSERT INTO `won` (`awardID`, `movieID`) VALUES ('6', '5');
 INSERT INTO `won` (`awardID`, `movieID`) VALUES ('4', '6');
 
-#need to do workers, and livesAt2 then livesAt1 convert into script
+#Fill lives_at1`
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Bala.Stella@hotmail.com",      "Beechcroft Court",  "24736");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Cecilie.Humaira@gmail.com",  "Benmore View" ,  "24724");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Cecilie.Lainey@hotmail.com",  "Berryhill Avenue",  "24738");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Clio.Seraphine@hotmail.com",  "Canon Place",   "24737");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Desmond.Stella@hotmail.com",  "Dolphin Gardens East", "24719");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Elli.Nerthus@hotmail.com",  "Fish Rake Lane",  "24726");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Emmett.Humaira@hotmail.com",  "Kelton Grove",   "24714");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Geoff.Humaira@gmail.com",   "Knowle Top Road",  "24729");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Ian.Lainey@yahoo.com",   "Maryfield Park",  "24732");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Juan.Eseoghene@hotmail.com",  "Merleswen",   "24716");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Juan.Lainey@yahoo.com" ,  "New Close Farm Road", "24715");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Kekepania.Seraphine@yahoo.com", "Stoneycrof Crescent", "24733");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Pavel.America@yahoo.com",   "Upper Piccadilly",  "24712");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Sudarshan.Lainey@gmail.com",  "Vandries Street",  "24701");
+INSERT INTO `lives_at1` (`email`,`street`,`zip`) VALUES ("Zarko.Ghislaine@yahoo.com",  "Venette Close",  "24731");
+#Fill workers
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14445', 'Joshua', '1', '0');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14446', 'Daniel', '0', '1');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14447', 'Sarah', '0', '1');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14448', 'Claire', '1', '0');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14449', 'Sanders', '1', '0');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14550', 'Calli', '1', '0');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14551', 'Robert', '0', '1');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14552', 'Ronald', '1', '0');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14553', 'David', '1', '0');
+INSERT INTO `workers` (`workerID`, `wname`, `isActor`, `isDirector`) VALUES ('14554', 'Madonna', '0', '1');
+#Fill lives_at2
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14445','280th Ave', '32819');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14446','Delaware St', '32852');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14447','Pennsylvania St', '29183');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14448','Burroughs Creek Road', '40212');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14449','Lakeside Dr', '23492');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14550','Hills Dr', '92092');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14551','Meadowoak Ln', '48213');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14552','Mansfield Rd', '12523');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14553','Commonwealth Dr', '24632');
+INSERT INTO `lives_at2` (`workerID`,`street`,`zip`) VALUES ('14554','Vera Ln', '92301');
+#Fill works_on
+INSERT INTO `works_on` (`movieID`, `workerID`) VALUES ('1', '14445');
+INSERT INTO `works_on` (`movieID`, `workerID`) VALUES ('2', '14446');
+INSERT INTO `works_on` (`movieID`, `workerID`) VALUES ('3', '14447');
+INSERT INTO `works_on` (`movieID`, `workerID`) VALUES ('4', '14448');
+INSERT INTO `works_on` (`movieID`, `workerID`) VALUES ('5', '14449');
+INSERT INTO `works_on` (`movieID`, `workerID`) VALUES ('6', '14550');
+
+
+ALTER TABLE users ADD COLUMN numRentalsAvailable INTEGER AFTER isAdmin;
+
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '8' WHERE (`email` = 'Bala.Stella@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '8' WHERE (`email` = 'Cecilie.Humaira@gmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '7' WHERE (`email` = 'Cecilie.Lainey@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '4' WHERE (`email` = 'Clio.Seraphine@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '3' WHERE (`email` = 'Desmond.Stella@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '2' WHERE (`email` = 'Elli.Nerthus@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '1' WHERE (`email` = 'Emmett.Humaira@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '10' WHERE (`email` = 'Geoff.Humaira@gmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '2' WHERE (`email` = 'Ian.Lainey@yahoo.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '3' WHERE (`email` = 'Juan.Eseoghene@hotmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '4' WHERE (`email` = 'Juan.Lainey@yahoo.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '5' WHERE (`email` = 'Kekepania.Seraphine@yahoo.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '2' WHERE (`email` = 'Pavel.America@yahoo.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '3' WHERE (`email` = 'Sudarshan.Lainey@gmail.com');
+UPDATE `dbproject`.`users` SET `numRentalsAvailable` = '0' WHERE (`email` = 'Zarko.Ghislaine@yahoo.com');
